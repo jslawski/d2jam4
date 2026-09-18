bfd7e77e379a3e738cc5acb4edf56ac9597cdaa8
Jared Slawski <jaredslawski@gmail.com>
2026-09-17 18:57:43 -0600

    Modifying noise gate and max volume stuff

---
+++ Assets/_Scripts/MicMeter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MicMeter : MonoBehaviour
+{
+    
+    private Image _micImage;
+
+    private void Awake()
+    {
+        this._micImage = GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogError("Loudness: " + MicrophoneManager.instance.GetNormalizedLoudness());    
+    
+        this._micImage.fillAmount = MicrophoneManager.instance.GetNormalizedLoudness();
+
+        if (this._micImage.fillAmount < 1.0f)
+        {
+            this._micImage.color = Color.red;
+        }
+        else
+        {
+            this._micImage.color = Color.green;
+        }
+    }
+    
+}
